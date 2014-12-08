ae4b8b8c2dd061f1b31b25ecaf709b7794c8250c
Loïc Haas <loic@haas.link>
2014-12-08 13:39:07 +0100

    Ajout des commentaires pour les Wrappers
    Former-commit-id: 5f569955537e29e16ca68e0bee8b9096729fed4f

--- Labo 4/Sources/Lab 4/RoadNetworkWrappers.h
+++ Labo 4/Sources/Lab 4/RoadNetworkWrappers.h
@@ -11,6 +11,7 @@
 #include "EdgeWeightedDigraph.h"
 #include "EdgeWeightedGraph.h"
 
+// Tout les wrapper de RoadNetwork herite de cette class
 class RoadNetworkWrapperBase {
 protected:
     const RoadNetwork& network;
@@ -21,21 +22,26 @@ public:
     }
 };
 
+// tout les wrapper de graph orienté herite de cette class
 class RoadNetworkDigraphWrapper : public RoadNetworkWrapperBase {
 protected:
     RoadNetworkDigraphWrapper(const RoadNetwork& network) : 
         RoadNetworkWrapperBase(network) {}
 
 public:
-    //typedef double Weight;Weight
+    // Type de Edge (oriente)
     typedef typename ASD2::WeightedDirectedEdge<double> Edge;
     
+    // Le poids doit être implementé par les class enfants
     virtual double getWeight(const RoadNetwork::Road& road) const = 0;
 
     template<typename Func>
     void forEachEdge(Func f) const {
         for (const RoadNetwork::Road& road : network.roads) {
             double w = getWeight(road);
+            
+            // Une route ca dans les deux sens (dans notre cas mais dans le cas general
+            // il est possible qu'il y ai des sens unique)
             Edge a(road.cities.first , road.cities.second, w);
             Edge b(road.cities.second, road.cities.first,  w);
             f(a);
@@ -45,9 +51,11 @@ public:
 
     template<typename Func>
     void forEachAdjacentEdge(int ville, Func f) const {
+        // Les Edge adjassent sont les routes qui partes de la ville
         for (int roadid : network.cities.at(ville).roads) {
             const RoadNetwork::Road& road = network.roads[roadid];
             int from, to;
+           
             if (road.cities.first == ville) {
                 from = road.cities.first;
                 to = road.cities.second;
@@ -55,6 +63,7 @@ public:
                 from = road.cities.second;
                 to = road.cities.first;
             }
+
             Edge e(from, to, getWeight(road));
             f(e);
         }
@@ -68,14 +77,16 @@ public:
     }
 };
 
+// Les Wrapper de graph non oriente herittent de cette class
 class RoadNetworGraphkWrapper : public RoadNetworkWrapperBase {
 protected :
     RoadNetworGraphkWrapper(const RoadNetwork& network) : 
         RoadNetworkWrapperBase(network) {}
 public:
-    //typedef double Weight;Weight
+    // type de Edge (non oriente)
     typedef typename ASD2::WeightedEdge<double> Edge;
     
+    // le poid des egres doit etre calculé par les classs enfants
     virtual double getWeight(const RoadNetwork::Road& road) const = 0;
 
     template<typename Func>
@@ -102,40 +113,60 @@ public:
     }
 };
 
+/*
+ * Class wrapper de RoadNetwork les Weight sera le prix de rénovation de la route
+ */
 class RoadNetworkWrapperPrice : public RoadNetworGraphkWrapper {
 public:
+     // Prix par km pour renover une autoroute en M
     const double MOTORWAY_PRICE = 15.;
+    
+    // Prix par mk pour renover une route normal en M
     const double NORMALWAY_PRICE = 7.;
     
     RoadNetworkWrapperPrice(const RoadNetwork& n) : RoadNetworGraphkWrapper(n) { }
     
     double getWeight(const RoadNetwork::Road& road) const override {
-        double mororway = road.motorway.Value();
+        // Proportion d'autoroute
+        double mororway = road.motorway.Value(); 
+        // Prix de renovation de la partie autoroute
         double motorWayPrice = (mororway * road.lenght) * MOTORWAY_PRICE;
+        // Prix de renovation de la partie route normal
         double normalWayPrice = ((1 - mororway) * road.lenght) * NORMALWAY_PRICE;
+        // Retoure le prix total
         return motorWayPrice + normalWayPrice;
     }
 };
 
+/*
+ * Wrapper qui a comme poid la longeur de la route
+ */
 class RoadNetworkWrapperShortest : public RoadNetworkDigraphWrapper {
 public:
     RoadNetworkWrapperShortest(const RoadNetwork& n) : RoadNetworkDigraphWrapper(n) { }
     
     double getWeight(const RoadNetwork::Road& road) const override {
-        //std::cout << "getWeight" << road.lenght << std::endl;
         return road.lenght;
     }
 };
 
+/*
+ * Wrapper qui a comme poid le temps de trajet
+ */
 class RoadNetworkWrapperFastest : public RoadNetworkDigraphWrapper {
 public:
     RoadNetworkWrapperFastest(const RoadNetwork& n) : RoadNetworkDigraphWrapper(n) { }
     
+    // Vitesse sur les autoroutes
     const double MOTORWAY_SPEED = 120.;
+    // Vitesse sur les routes normal
     const double NORMALWAY_SPEED = 70.;
     double getWeight(const RoadNetwork::Road& road) const override {
+        // Proportion d autoroute
         double mororway = road.motorway.Value();
+        // Temps passe sur les autoroute en H
         double motorWaytime = (mororway * road.lenght) / MOTORWAY_SPEED;
+        // temps passe sur les route normal en H
         double normalWaytime = ((1 - mororway) * road.lenght) / NORMALWAY_SPEED;
         return motorWaytime + normalWaytime;
     }
