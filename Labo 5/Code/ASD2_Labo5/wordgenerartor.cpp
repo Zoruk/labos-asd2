d1ca8e78553994e1979b80090addff5655e91676
Loïc Haas <loic@haas.link>
2014-12-20 22:25:22 +0100

    Update git ignore for *.user files
    Former-commit-id: 1166c11ad617e2910bf862007fb7aad6245fa6ce

---
+++ Labo 5/Code/ASD2_Labo5/wordgenerartor.cpp
@@ -0,0 +1,5 @@
+#include "wordgenerartor.h"
+
+WordGenerartor::WordGenerartor()
+{
+}
---
+++ Labo 5/Code/ASD2_Labo5/wordgenerartor.h
@@ -0,0 +1,10 @@
+#ifndef WORDGENERARTOR_H
+#define WORDGENERARTOR_H
+
+class WordGenerartor
+{
+public:
+    WordGenerartor();
+};
+
+#endif // WORDGENERARTOR_H
