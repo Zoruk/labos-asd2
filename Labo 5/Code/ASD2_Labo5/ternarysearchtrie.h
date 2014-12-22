4c2caf3cb16b131a3980292cdb9315e21fec08d7
Loïc Haas <loic@haas.link>
2014-12-22 23:21:28 +0100

    Some more clean up
    Former-commit-id: 4c80a456f21f5ea28aceda8db69a06d8e4162eb4

--- Labo 5/Code/ASD2_Labo5/ternarysearchtrie.h
+++ Labo 5/Code/ASD2_Labo5/ternarysearchtrie.h
@@ -9,94 +9,28 @@ private:
     public:
         char value;
         Node* right;  // sous arbre avec des cles plus grandes
-        Node* center;
+        Node* center; //
         Node* left;   // sous arbre avec des cles plus petites
         int nodeHeight; // hauteur du sous-arbre dont ce noeud est la racine.
         bool isEnd;
         Node(char value) : value(value), right(nullptr), center(nullptr), left(nullptr), nodeHeight(0), isEnd(false) { }
     };
 
-    //
     // Racine de l'arbre.
-    //
     Node* root;
+
+    // On ne peux pas traiter la taille de la meme manière que ceux des arbres avl
     size_t _size;
-
-    // HELPER: Methode aide pour le methodes d'effacement. Efface del et retourne ret.
-    Node* deleteAndReturn( Node* del, Node* ret);
-
-    // HELPER: Mise à jour de la hauteur d'un sous-arbre à partir des hauteurs de ses enfants
-    void updateNodeHeight(Node* x);
-
-private:
-    //
-    // AVL: rotation droite avec mise à jour des tailles et hauteurs
-    //
-    Node* rotateRight(Node* x);
-    //
-    // AVL: rotation gauche avec mise à jour des tailles et hauteurs
-    //
-    Node* rotateLeft(Node* x);
-
-    //
-    // Constructeur. La racine est vide
-    //
 public:
     TernarySearchTrie();
-
-    //
-    // Destructeur.
-    //
-public:
     ~TernarySearchTrie();
 
-private:
-    void deleteSubTree(Node* x);
-
-    //
-    // Nombre d'elements
-    //
-public:
+    void put( const char* key);
+    void deleteElement( const char* key);
+    int height();
+    bool contains(const char* key) const;
     size_t size() const;
 
-
-    //
-    // Insertion d'une chaine
-    //
-public:
-    void put( const char* key);
-
-private:
-    Node* put(Node* x, const char* str);
-
-    int balance(Node* x);
-
-    Node* restoreBalance(Node* x);
-
-public:
-    bool contains(const char* key) const;
-private:
-    bool contains(Node* x,  const char* key ) const;
-
-    //
-    // Profondeur de l'arbre.
-    //
-public:
-    int height();
-private:
-    int height(Node* x);
-
-    //
-    // Efface l'element de cle key
-    //
-public:
-
-    void deleteElement( const char* key);
-
-private:
-    Node* deleteElement( Node* x, const char* key);
-
-public:
     template < typename Fn >
     void visitInOrder ( Fn f) {
         if ( root != nullptr)
@@ -104,6 +38,24 @@ public:
     }
 
 private:
+    int height(Node* x);
+    bool contains(Node* x,  const char* key ) const;
+    int balance(Node* x);
+
+    Node* restoreBalance(Node* x);
+    Node* rotateRight(Node* x);
+    Node* rotateLeft(Node* x);
+    Node* deleteElement( Node* x, const char* key);
+    Node* put(Node* x, const char* str);
+
+    void deleteSubTree(Node* x);
+
+    // HELPER: Methode aide pour le methodes d'effacement. Efface del et retourne ret.
+    Node* deleteAndReturn( Node* del, Node* ret);
+
+    // HELPER: Mise à jour de la hauteur d'un sous-arbre à partir des hauteurs de ses enfants
+    void updateNodeHeight(Node* x);
+
     template < typename Fn >
     void visitInOrder ( Node* x, Fn f) {
         if (x->left != nullptr)
